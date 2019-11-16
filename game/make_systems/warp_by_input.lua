45e266ef2bf04bcc32aef1ebfb17fe42652f8704
Oliver Vartiainen <oliver@iki.fi>
2019-11-16 21:52:48 +0200

    Add some TODOs

--- game/make_systems/warp_by_input.lua
+++ game/make_systems/warp_by_input.lua
@@ -10,7 +10,7 @@ return function(_, entity_manager, player_input)
       if do_warp then
          local input_entity_id = entity_manager:get_unique_component("input")
          local position_c = entity_manager:get_component(input_entity_id, "position")
-         local new_level = position_c.level == "temple" and "dungeon" or "temple"
+         local new_level = position_c.level == "temple" and "dungeon" or "temple" -- TODO: Dehardcore "temple" and "dungeon"
          entity_manager:update_component(input_entity_id, position_c, { level = new_level })
 
          do_warp = false
