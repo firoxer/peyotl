872718d090531f1672f10fc6fa40ea5cfc81183c
Oliver Vartiainen <oliver@iki.fi>
2019-11-24 20:20:06 +0200

    Rewrite monster generation into monster spawning

--- game/generate.lua
+++ game/generate.lua
@@ -1,15 +1,9 @@
 local generate_gems = require("game.generate.generate_gems")
-local generate_monsters = require("game.generate.generate_monsters")
 local generate_player = require("game.generate.generate_player")
 local generate_tiles = require("game.generate.generate_tiles")
 
 return function(entity_manager, config)
    generate_tiles(entity_manager, config.levels)
-   local player_id = generate_player(entity_manager, config.player)
-   generate_monsters(entity_manager, config.levels)
+   generate_player(entity_manager, config.player)
    generate_gems(entity_manager, config.levels)
-
-   return {
-      player_id = player_id
-   }
 end
--- game/make_update.lua
+++ game/make_update.lua
@@ -3,6 +3,7 @@ local make_attack = require("game.make_systems.attack")
 local make_death = require("game.make_systems.death")
 local make_move_by_input = require("game.make_systems.move_by_input")
 local make_warp_by_input = require("game.make_systems.warp_by_input")
+local make_spawn_monsters = require("game.make_systems.spawn_monsters")
 
 return function(levels_config, entity_manager, player_input)
    local chase = make_chase(levels_config, entity_manager)
@@ -10,6 +11,7 @@ return function(levels_config, entity_manager, player_input)
    local death = make_death(levels_config, entity_manager)
    local move_by_input = make_move_by_input(levels_config, entity_manager, player_input)
    local warp_by_input = make_warp_by_input(levels_config, entity_manager, player_input)
+   local spawn_monsters = make_spawn_monsters(levels_config, entity_manager)
 
    return function(dt)
       chase(dt)
@@ -17,5 +19,6 @@ return function(levels_config, entity_manager, player_input)
       death()
       move_by_input()
       warp_by_input()
+      spawn_monsters()
    end
 end
