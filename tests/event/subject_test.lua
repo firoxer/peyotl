d4f0cc1d9bca50298042ffa5fcecc4946c1608a5
Oliver Vartiainen <oliver@iki.fi>
2019-12-02 18:03:34 +0200

    Add tests for event.Subject

--- tests/event/subject_test.lua
+++ tests/event/subject_test.lua
@@ -1,1 +1,41 @@
--- TODO
+local Subject = require("game.event.subject")
+
+do
+   local s = Subject.new()
+
+   local event_fired = false
+   s:subscribe('test_event', function(event)
+      event_fired = true
+      assert(event.data == 'test_event_data')
+   end)
+
+   s:notify('test_event', { data = 'test_event_data' })
+
+   assert(event_fired)
+end
+
+do
+   local s = Subject.new()
+
+   local event_fired = false
+   s:subscribe('test_event_a', function()
+      event_fired = true
+   end)
+
+   s:notify('test_event_b', nil)
+
+   assert(not event_fired)
+end
+
+do
+   local s = Subject.new()
+
+   local event_fired = false
+   s:subscribe_all(function()
+      event_fired = true
+   end)
+
+   s:notify('test_event', nil)
+
+   assert(event_fired)
+end
