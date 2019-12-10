a5a78cd0d26853655bd381ce94fa2eddf2e85cde
Oliver Vartiainen <oliver.vartiainen@kodan.fi>
2019-12-10 22:17:03 +0200

    Split code from init.lua to separate files

---
+++ game/util/assertx.lua
@@ -0,0 +1,40 @@
+return {
+   is_boolean =
+      require("game.util.assertx.is_boolean"),
+
+   is_boolean_or_nil =
+      require("game.util.assertx.is_boolean_or_nil"),
+
+   is_function =
+      require("game.util.assertx.is_function"),
+
+   is_function_or_nil =
+      require("game.util.assertx.is_function_or_nil"),
+
+   is_instance_of =
+      require("game.util.assertx.is_instance_of"),
+
+   is_instance_of_or_nil =
+      require("game.util.assertx.is_instance_of_or_nil"),
+
+   is_number =
+      require("game.util.assertx.is_number"),
+
+   is_number_or_nil =
+      require("game.util.assertx.is_number_or_nil"),
+
+   is_string =
+      require("game.util.assertx.is_string"),
+
+   is_string_or_nil =
+      require("game.util.assertx.is_string_or_nil"),
+
+   is_table =
+      require("game.util.assertx.is_table"),
+
+   is_table_or_nil =
+      require("game.util.assertx.is_table_or_nil"),
+
+   is_true =
+      require("game.util.assertx.is_true"),
+}
---
+++ game/util/tablex.lua
@@ -0,0 +1,31 @@
+return {
+   clone =
+      require("game.util.tablex.clone"),
+
+   contains =
+      require("game.util.tablex.contains"),
+
+   describe =
+      require("game.util.tablex.describe"),
+
+   mappairs =
+      require("game.util.tablex.mappairs"),
+
+   merge =
+      require("game.util.tablex.merge"),
+
+   readonly =
+      require("game.util.tablex.readonly"),
+
+   reverse =
+      require("game.util.tablex.reverse"),
+
+   sample =
+      require("game.util.tablex.sample"),
+
+   shuffle =
+      require("game.util.tablex.shuffle"),
+
+   uptight =
+      require("game.util.tablex.uptight"),
+}
