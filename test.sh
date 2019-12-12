50851a154eb4bb2b78bdd62e93e4905bc722cb92
Oliver Vartiainen <oliver.vartiainen@kodan.fi>
2019-12-13 01:12:52 +0200

    Make tests closer to what's ran in love

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 
   for file in $files; do
       echo "testing $file"
-      lua5.3 -l init $file
+      lua5.2 -l init $file
       if [ $? -ne 0 ]; then
           echo "error"
           exit 1
@@ -17,7 +17,7 @@ if [ $# -eq 0 ]; then
   echo "all tests passed"
 elif [ $# -eq 1 ]; then
     echo "testing $1"
-    lua5.3 -l init $1
+    lua5.2 -l init $1
     if [ $? -ne 0 ]; then
         echo "error"
         exit 1
