8291d2646eaab71dec8caf1927eef0c6900332cc
Oliver Vartiainen <oliver.vartiainen@kodan.fi>
2019-12-16 23:04:01 +0200

    Disable love2d things in conf.lua

--- conf.lua
+++ conf.lua
@@ -1,4 +1,12 @@
 function love.conf(t)
     -- For logging to work on Windows
    t.console = true
+
+   -- Disabling things we don't need
+   t.modules.data = false
+   t.modules.joystick = false
+   t.modules.physics = false
+   t.modules.thread = false
+   t.modules.touch = false
+   t.modules.video = false
 end
