93d1c893209949292d14799cf219c9dbd67ff5e3
Oliver Vartiainen <oliver.vartiainen@kodan.fi>
2020-01-03 09:09:32 +0200

    Remove an ununsed function from altar generation

--- game/generate/generate_altars.lua
+++ game/generate/generate_altars.lua
@@ -1,12 +1,6 @@
 local create_component = require("game.entity.create_component")
 local tileset_quad_names = require("game.render.tileset_quad_names")
 
-local function generate_altar(entity_manager, level_name, point)
-   local altar_id = entity_manager:new_entity_id()
-   entity_manager:add_component(altar_id, create_component.position(level_name, point))
-   entity_manager:add_component(altar_id, create_component.render(tileset_quad_names.temple_altar, 1))
-end
-
 return function(entity_manager, levels_config)
    for level_name, level_config in pairs(levels_config) do
       if level_config.altar then
