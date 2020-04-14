fc3ae86a4cc8fb2439dd77cc089edc51b1a2056e
Oliver Vartiainen <oliver.vartiainen@kodan.fi>
2020-04-14 13:17:48 +0300

    Make indentation clearer in util.log

--- game/util/log.lua
+++ game/util/log.lua
@@ -1,28 +1,29 @@
-local level_headers = love.system.getOS() == "Linux"
-   and {
-      debug =
-         "   [\27[37mdebug\27[0m]",
-      info =
-         "    [\27[34minfo\27[0m]",
-      warn =
-         "    [\27[33mwarn\27[0m]",
-      error =
-         "   [\27[31merror\27[0m]",
-      fatal =
-         "   [\27[37;1m\27[41;1mfatal\27[0m]",
-   }
-   or {
-      debug =
-         "   [debug]",
-      info =
-         "    [info]",
-      warn =
-         "    [warn]",
-      error =
-         "   [error]",
-      fatal =
-         "   [fatal]",
-   }
+local level_headers =
+   love.system.getOS() == "Linux"
+      and {
+         debug =
+            "   [\27[37mdebug\27[0m]",
+         info =
+            "    [\27[34minfo\27[0m]",
+         warn =
+            "    [\27[33mwarn\27[0m]",
+         error =
+            "   [\27[31merror\27[0m]",
+         fatal =
+            "   [\27[37;1m\27[41;1mfatal\27[0m]",
+      }
+      or {
+         debug =
+            "   [debug]",
+         info =
+            "    [info]",
+         warn =
+            "    [warn]",
+         error =
+            "   [error]",
+         fatal =
+            "   [fatal]",
+      }
 
 return tablex.mappairs(level_headers, function(_, header)
    return function(message, data)
